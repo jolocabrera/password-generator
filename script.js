72daa166408cce5781da71743e33566d3b58d9f4
Jolo <jolocabrera@yahoo.com>
2022-04-11 13:32:59 -0700

    add upper case letters to password and created generator base

--- script.js
+++ script.js
@@ -2,16 +2,30 @@
 
 //Whatever generatePassword function returns will display on screen in main card
 function generatePassword() {
-  var passwordLength = window.prompt("How long would you like your password to be? Select a number between 8 and 128.");
-  if (passwordLength >= 8 && passwordLength <= 128) {
-    window.alert("Your password will be " + passwordLength + " characters long.")
-  }
-  else {
-    window.alert("Please choose a number between 8 and 128.");
-    generatePassword();
-  }
-  return passwordLength;
+  var passwordHolder = ""
+  var chars = ""
 
+  //sets password length
+  var passwordLength = window.prompt("How long would you like your password to be? Select a number between 8 and 128.");
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      window.alert("Your password will be " + passwordLength + " characters long.")
+    }
+    else {
+      window.alert("Please choose a number between 8 and 128.");
+      generatePassword();
+    }
+  
+  //selecting character sets (case, numbers, special characters)
+  // upper case letters
+  var uppercase = window.confirm("Include uppercase letters?");
+  if (uppercase) {
+    chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  for (i = 0; i < passwordLength; i++) {
+    passwordHolder += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return passwordHolder;
 }
 
 // Get references to the #generate element
