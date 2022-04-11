d1b6684b4f25260b679936f9a1aa69aa6acfc6cf
Jolo <jolocabrera@yahoo.com>
2022-04-11 12:42:18 -0700

    add length criteria to generator

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Assignment code here
 
+//Whatever generatePassword function returns will display on screen in main card
+function generatePassword() {
+  var passwordLength = window.prompt("How long would you like your password to be? Select a number between 8 and 128.");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    window.alert("Your password will be " + passwordLength + " characters long.")
+  }
+  else {
+    window.alert("Please choose a number between 8 and 128.");
+    generatePassword();
+  }
+  return passwordLength;
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
